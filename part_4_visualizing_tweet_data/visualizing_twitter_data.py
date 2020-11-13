996ddbc0162e77835f41b7afe233c98850f3288c
salway-dolkar <salway.333@cet.ac.in>
2020-11-13 23:11:00 +0100

    Used time layered graphs to assist visualisation

--- part_4_visualizing_tweet_data/visualizing_twitter_data.py
+++ part_4_visualizing_tweet_data/visualizing_twitter_data.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # # # # TWITTER CLIENT # # # #
 class TwitterClient():
     def __init__(self, twitter_user=None):
@@ -149,7 +148,7 @@ if __name__ == '__main__':
     time_likes = pd.Series(data=df['likes'].values, index=df['date'])
     time_likes.plot(figsize=(16, 4), label="likes", legend=True)
 
-    #time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
+    time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
     time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
     plt.show()
 
